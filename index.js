f01de4b45fc949182383bf6cf11647d7130b86f4
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-25 14:42:29 -0700

    oh boy this one was hard but got it to read then write a file

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const fs = require('fs');
-
+const { join } = require('path');
 // fs.readFile('./hi', { encoding: 'utf8' }, (err, data) => {
 //   if(err) return console.error(err);
 
 //   console.log(data);
 // });
 
-const data = 'here is ralphy the damn dog!';
-fs.writeFile('message.txt', data, (err) => {
-  if(err) console.error(err);
-  console.log('The file has been saved!');
-});
+// const data = 'here is ralphy the damn dog!';
+// fs.writeFile('message.txt', data, (err) => {
+//   if(err) console.error(err);
 
+//   fs.readFile('./message.txt', { encoding: 'utf8' }, (err, data) => {
+//     if(err) return console.error(err);
+
+//     console.log(data);
+//   });
+
+// });
+fs.readFile(join('./message.txt'), { encoding: 'utf8' }, (err, data) => {
+  if(err) return console.error(err);
+  console.log(data);
+
+  fs.writeFile(join('message2.txt'), data, (err) => {
+    if(err) return console.error(err);
+
+  });
+})
