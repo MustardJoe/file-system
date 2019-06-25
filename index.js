f6444940d35d18c584be791cf8e69cca70cf2ae2
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-25 13:54:56 -0700

    commented out old func and wrote file write func

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
-fs.readFile('./README.md', { encoding: 'utf8' }, (err, data) => {
-  if(err) return console.error(err);
 
-  console.log(data);
+// fs.readFile('./hi', { encoding: 'utf8' }, (err, data) => {
+//   if(err) return console.error(err);
+
+//   console.log(data);
+// });
+
+const data = 'here is ralphy the damn dog!';
+fs.writeFile('message.txt', data, (err) => {
+  if(err) console.error(err);
+  console.log('The file has been saved!');
 });
+
